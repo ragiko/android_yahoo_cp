101681d54208a21e092479bbe9a7c563ce4f6203
Taguchi Hiroaki <ragiko.complexe@gmail.com>
2014-11-24 13:24:43 +0900

    textでタイトルを動的に表示完了 (表示ミスのバグあり)

--- src/com/parse/mealspotting/TextActivity.java
+++ src/com/parse/mealspotting/TextActivity.java
@@ -2,15 +2,53 @@ package com.parse.mealspotting;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
 
 public class TextActivity extends Activity {
 	// TODO: idからbackgroundでゲットしてくる
+	
+	private ParseObject text;
+	private String textId;
+	
+	private class RemoteDataTask extends AsyncTask<Void, Void, Void> {
+		// Override this method to do custom remote calls
+		protected Void doInBackground(Void... params) {
+			// Gets the current list of todos in sorted order
+			ParseQuery<ParseObject> query = new ParseQuery<ParseObject>("Text");
+			query.whereEqualTo("objectId", textId);
+
+			try {
+				text = query.getFirst();
+			} catch (ParseException e) {
+
+			}
+			return null;
+		}
+
+		@Override
+		protected void onPreExecute() {
+			super.onPreExecute();
+		}
+
+		@Override
+		protected void onProgressUpdate(Void... values) {
+			super.onProgressUpdate(values);
+		}
+
+		@Override
+		protected void onPostExecute(Void result) {
+			Log.d("title", (String) text.get("title"));
+			TextView titleTextView = (TextView) findViewById(R.id.title);
+			titleTextView.setText((String) text.get("title"));
+		}
+	}
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -18,7 +56,10 @@ public class TextActivity extends Activity {
 		setContentView(R.layout.activity_text);	
 		
 		Intent i = getIntent();
-		String textId = i.getStringExtra("text_id");
-		Log.d("a", textId);
+		textId = i.getStringExtra("text_id");
+		
+		
+		
+		new RemoteDataTask().execute();
 	}
 }
